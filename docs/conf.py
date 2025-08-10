79e6a5455052fd3eebe72a520cdc393e7e6c3a49
myxie <contact@ryanbunney.com>
2025-08-10 13:12:40 +0800

    #48: Add conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ autosummary_generate = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_Theme'
+html_theme = 'sphinx_rtd_theme'
 
 # html_theme_options = {"full_logo":"true"}
 html_theme_options = {
