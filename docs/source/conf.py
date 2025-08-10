dc968f3f011e1d8b9eb0de5fe3547722becae1a9
myxie <contact@ryanbunney.com>
2025-08-10 13:50:58 +0800

    #48: Add custom.css
    Signed-off-by: myxie <contact@ryanbunney.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -62,6 +62,7 @@ html_logo = '' #'new_logo.svg'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
-
+html_static_path = ['_static']
+html_css_files = ["css/custom.css"]
+html_style = "css/custom.css"
 html_title = 'SKAWorkflows'
